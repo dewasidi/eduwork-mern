e6a0d16198711ac8525785acb0b912a0d4cfe841
dewa sidi <purnamasidi26@gmail.com>
2022-09-02 15:31:50 +0800

    add callback

---
+++ asynchronus/script.js
@@ -0,0 +1,32 @@
+function getData(url,cb) {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function (){
+        if (xhr.status === 200){
+            return cb (JSON.parse(xhr.responseText)) ;
+        }
+    };
+    xhr.open("GET", url);
+xhr.send();
+}
+const data = getData("https://jsonplaceholder.typicode.com/users", function(data){
+    ekstrakData(data);
+});
+
+function ekstrakData (data){
+    const tabel = document.getElementById('data-table');
+    for (const dataUser of data){
+        const row = `
+                        <tr>
+                            <td>${dataUser.id}</td>
+                            <td>${dataUser.username}</td>
+                            <td>${dataUser.email}</td>
+                            <td>${dataUser.addres['name']}</td>
+                            <td>${dataUser.company['name']}</td>
+                        </tr>
+                    `
+                    tabel.innerHTML += row
+    }
+};
+
+
+
