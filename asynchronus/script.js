d198a5f88664f93ef74cf8a96c0f88d97420c0f3
dewa sidi <purnamasidi26@gmail.com>
2022-09-12 22:14:49 +0800

    merge cinflict

--- asynchronus/script.js
+++ asynchronus/script.js
@@ -1,18 +1,3 @@
-
-// function getData(url,cb) {
-//     const xhr = new XMLHttpRequest();
-//     xhr.onload = function (){
-//         if (xhr.status === 200){
-//             return cb (JSON.parse(xhr.responseText)) ;
-//         }
-//     };
-//     xhr.open("GET", url);
-// xhr.send();
-// }
-// const data = getData("https://jsonplaceholder.typicode.com/users", function(data){
-//     ekstrakData(data);
-// });
-
 const data = new XMLHttpRequest();
 data.open('GET', 'https://jsonplaceholder.typicode.com/users');
 data.addEventListener("load", function (){
@@ -38,29 +23,4 @@ data.addEventListener("load", function (){
         alert("NA")
         }
     });
-
-
-    //const tabel = document.getElementById('name');
-    //tabel.textContent = json.name;
-    //document.body.appendChild(tabel);
-    
-
 data.send();
-// function ekstrakData (data){
-//     const tabel = document.getElementById('data-table');
-//     for (const dataUser of data){
-//         const row = `
-//                         <tr>
-//                             <td>${dataUser.id}</td>
-//                             <td>${dataUser.username}</td>
-//                             <td>${dataUser.email}</td>
-//                             <td>${dataUser.addres['name']}</td>
-//                             <td>${dataUser.company['name']}</td>
-//                         </tr>
-//                     `
-//                     tabel.innerHTML += row
-//     }
-// };
-
-// ekstrakData(data);
-
